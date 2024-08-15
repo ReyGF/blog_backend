3a2eb5cd62c48a058bea232856fab19d0333a450
ReyGF <gomezreynol14@gmail.com>
2024-08-15 14:35:32 -0400

    create db.module

---
+++ src/db/db.service.ts
@@ -0,0 +1,9 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common"
+import { PrismaClient } from "@prisma/client"
+
+@Injectable()
+export class DbService extends PrismaClient implements OnModuleInit{
+    async onModuleInit() {
+        await this.$connect()
+    }
+}
